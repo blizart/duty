7a0ef24892e2eae6d8b7215c67ee1e895eb87b71
blizart <blizart.kontakt@gmail.com>
2014-01-04 15:37:51 +0100

    remove ckeditor

--- app/overrides/change_logo.rb
+++
@@ -1,4 +0,0 @@
-#Deface::Override.new(:virtual_path => 'spree/shared_header',
-#                     :name => 'change-logo',
-#                     :replace => "erb[loud]:contains('text_field :price')",
-#                     :text => "")
